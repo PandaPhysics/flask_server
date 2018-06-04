1b3cb2477bfdc9294a342d4f0c6a97c9e1928270
Sid Narayanan <sidn@mit.edu>
2018-06-04 14:34:31 -0400

    making generic

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash 
 
+export FLASK_BASE_DIR="/home/snarayan/flask_server/"
+
 # see if a process exists already
-if [[ $(ps U snarayan | grep flask | wc -l) > 1 ]]; then
-    pid=$(ps U snarayan | grep flask | grep -v grep | awk '{ print $1 }')
+if [[ $(ps U snarayan | grep "python.*flask" | wc -l) > 1 ]]; then
+    pid=$(ps U snarayan | grep "python.*flask" | grep -v grep | awk '{ print $1 }')
     echo -n "Stopping process $pid..."
     kill $pid
     echo " Done"
@@ -13,5 +15,5 @@ echo -n "Starting server..."
 wd=$PWD
 cd /cvmfs/cms.cern.ch/slc6_amd64_gcc630/cms/cmssw/CMSSW_9_4_6/; eval `scramv1 runtime -sh`; cd $wd # need python>=2.7.9
 export FLASK_APP=server.py
-python -m flask run --host=0.0.0.0 > /local/snarayan/logs/doxy.log 2>&1 & disown
+python -m flask run --host=0.0.0.0 > /local/snarayan/logs/flask.log 2>&1 & disown
 echo " Done"
--- server.py
+++ server.py
@@ -1,15 +1,76 @@
-from os import system, path
-from flask import request, Flask
-basedir = '/home/snarayan/doxy/repos/'
-app = Flask('doxy')
+from flask import request, Flask, abort, g
+from os import system, path, getenv
+import sqlite3
+import json
 
+basedir = getenv('FLASK_BASE_DIR') 
+app = Flask('')
+
+# actions to be taken on push to Panda* repositories
+# called through GitHub webhook
 @app.route('/push', methods=['POST'])
 def push():
+    doxy_basedir = basedir + '/doxy/repos/'
     data = request.get_json()
     repo = data['repository']['git_url'].split('/')[-1].replace('.git','')
-    if not path.isdir(basedir+repo):
-        system('cd %s; git lfs clone %s %s'%(basedir, data['repository']['clone_url'], repo))
-    system('cd %s/%s ; git lfs pull origin master'%(basedir,repo))
-    system('cd %s ; doxygen ../doxy.cfg'%basedir)
+    if not path.isdir(doxy_basedir+repo):
+        system('cd %s; git lfs clone %s %s'%(doxy_basedir, data['repository']['clone_url'], repo))
+    system('cd %s/%s ; git lfs pull origin master'%(doxy_basedir,repo))
+    system('cd %s ; doxygen ../doxy.cfg'%doxy_basedir)
     return 'Success!\n'
 
+
+# manage condor tasks through an sqlite3 db
+dbpath = basedir + '/condor/tasks.sqlite'
+exists = path.isfile(dbpath)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(dbpath)
+        if not exists:
+            cursor = db.cursor()
+            cursor.execute('CREATE TABLE jobs (task TEXT, arg TEXT, job_id TEXT, timestamp INTEGER)')
+            cursor.close()
+    return db 
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def query(cmd, args=()):
+    cur = get_db().execute(cmd, args)
+    rv = cur.fetchall()
+    cur.close()
+    return rv
+
+@app.route('/condor', methods=['GET', 'POST'])
+def condor():
+    if request.method == 'GET':
+        # query db and return
+        task = request.args.get('task')
+        if not task:
+            abort(402)
+        where = 'task="%s"'%task
+        jid = request.args.get('job_id')
+        if jid:
+            where += ', job_id=%i'%jid
+        cursor = get_db().execute('SELECT `arg`, `job_id`, `timestamp` FROM jobs WHERE %s;'%where)
+        payload = cursor.fetchall()
+        cursor.close()
+        return json.dumps(payload)
+    else:
+        # use JSON payload to fill table
+        data = request.get_json()
+        try:
+            task = data['task']
+            timestamp = data['timestamp']
+            job_id = data['job_id']
+            records = [(task, arg, job_id, timestamp) for arg in data['args']]
+            get_db().executemany('INSERT INTO jobs VALUES (?,?,?,?)', records)
+            get_db().commit()
+            return str(len(records))+'\n'
+        except KeyError:
+            abort(402)
