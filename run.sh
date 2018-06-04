e3c734cad1486e84fd2f959f80dc1c4dc2701fe7
Sid Narayanan <sidn@mit.edu>
2018-06-04 02:11:31 -0400

    better server start/stop

--- run.sh
+++ run.sh
@@ -1,1 +1,17 @@
-export FLASK_APP=server.py; python -m flask run --host=0.0.0.0
+#!/bin/bash 
+
+# see if a process exists already
+if [[ $(ps U snarayan | grep flask | wc -l) > 1 ]]; then
+    pid=$(ps U snarayan | grep flask | grep -v grep | awk '{ print $1 }')
+    echo -n "Stopping process $pid..."
+    kill $pid
+    echo " Done"
+    sleep 1
+fi
+
+echo -n "Starting server..."
+wd=$PWD
+cd /cvmfs/cms.cern.ch/slc6_amd64_gcc630/cms/cmssw/CMSSW_9_4_6/; eval `scramv1 runtime -sh`; cd $wd # need python>=2.7.9
+export FLASK_APP=server.py
+python -m flask run --host=0.0.0.0 > /local/snarayan/logs/doxy.log 2>&1 & disown
+echo " Done"
